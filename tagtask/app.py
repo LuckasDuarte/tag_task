d92f4fb4dc9e1cdd7c4ed04240dcce7cfdb51b3c
Lucas Duarte <luckasdu686@gmail.com>
2023-07-21 20:33:19 -0300

    criando HomePage

--- tagtask/app.py
+++ tagtask/app.py
@@ -1,10 +1,37 @@
 # --------------- Gerênciador de Tarefas - Tag Task --------------- #
 # --------------- Dev - Lucas Duarte Batista --------------- #
+
 # Instalar dependências => pip install -r requirements.txt
-import tkinter
+
+import tkinter as tk
+from tkinter import messagebox
 import customtkinter
 import openpyxl
 from PIL import ImageTk, Image
+
+# ------------------------------------------------------------------------------------------
+
+class TagTask:
+    def __init__(self):
+        self.home = tk.Tk()
+        self.home.title("Tag Task")
+        self.home.state('zoomed')
+
+
+        self.home.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.home.mainloop()
+
+
+
+    #verificação de fechamento de Página
+    def on_closing(self):
+        if messagebox.askokcancel("Fechar", "Deseja realmente sair?"):
+            self.home.destroy()
+
+
+Home = TagTask()
+
+
 
 
 
