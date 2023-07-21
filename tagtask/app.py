edc0477dffa1df73da2d4b3eb76edb4e8888a30c
Lucas Duarte <luckasdu686@gmail.com>
2023-07-21 20:27:18 -0300

    subindo repo

---
+++ tagtask/app.py
@@ -0,0 +1,11 @@
+# --------------- Gerênciador de Tarefas - Tag Task --------------- #
+# --------------- Dev - Lucas Duarte Batista --------------- #
+# Instalar dependências => pip install -r requirements.txt
+import tkinter
+import customtkinter
+import openpyxl
+from PIL import ImageTk, Image
+
+
+
+
